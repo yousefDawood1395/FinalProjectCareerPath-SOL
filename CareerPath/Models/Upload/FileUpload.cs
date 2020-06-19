0502dfe1aa3cddee7b42c4d2681e50efbab2695b
youssef <programmer.yousef@gmail.com>
2020-06-19 03:12:19 +0200

    done inserting image in wwwroot

---
+++ CareerPath/Models/Upload/FileUpload.cs
@@ -0,0 +1,14 @@
+﻿using CareerPath.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareerPath.Models.Upload
+{
+    public class FileUpload
+    {
+        public IFormFile Files { get; set; }
+    }
+}
